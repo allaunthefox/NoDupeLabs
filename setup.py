9553cddd0952ecfc0c5258024a2865794646565c
allaunthefox <bigdataiscoming+9i37y6j2@protonmail.com>
2025-12-13 03:37:32 -0600

    Fix packaging configuration and resolve .egg-info directory issues

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nodupe",
+    version="1.0.0",
+    description="Advanced duplicate file detection and management system",
+    author="NoDupeLabs",
+    packages=find_packages(where="."),
+    package_dir={"": "."},
+    include_package_data=True,
+    install_requires=[
+        # Add your dependencies here
+    ],
+    python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "nodupe=nodupe.core.main:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
